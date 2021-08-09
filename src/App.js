cde0068c99f46cfc1bf68566264ca6780b535860
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-09 17:28:24 +0300

    Board component structure the class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Board from "./components/board/Board";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Board></Board>
     </div>
   );
 }
---
+++ src/components/board/Board.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <h1>My TASKS LIST</h1>;
+  }
+}
