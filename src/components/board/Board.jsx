51d72f9567e8c2bfc838ca8e55787750c15f0951
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-09 18:21:14 +0300

    link between plus button and Add Task function

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -4,9 +4,17 @@ import Form from "../form/Form";
 export default class Board extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      text: "",
+    };
+  }
+  AddTask() {
+    let textVal = document.querySelector("#text_field").value;
+    console.log(textVal);
+    this.setState({ text: textVal });
+    // console.log(this.state.text);
   }
   render() {
-    return <Form></Form>;
+    return <Form add={this.AddTask.bind(this)}></Form>;
   }
 }
