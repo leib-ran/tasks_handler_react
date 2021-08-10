3b28dda053ba4f18f74b3f67037d8b2dbb497154
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-10 08:21:26 +0300

    v button works

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import Form from "../form/Form";
 import Task from "../tasks/tasks";
 
@@ -8,33 +8,57 @@ export default class Board extends React.Component {
     this.state = {
       text:
         localStorage["tasks"] == null ? [] : JSON.parse(localStorage["tasks"]),
+      done:
+        localStorage["done"] == null ? [] : JSON.parse(localStorage["done"]),
     };
+    this.signTask = this.signTask.bind(this);
   }
 
-  AddTask() {
+  addTask() {
     let textVal = document.querySelector("#text_field").value;
     let arrCopy = [...this.state.text];
-    this.setState({ text: arrCopy.concat(textVal) });
+    let arrDone = [...this.state.done];
+    this.setState({
+      text: arrCopy.concat(textVal),
+      done: arrDone.concat("false"),
+    });
     localStorage.setItem("tasks", JSON.stringify(arrCopy.concat(textVal)));
+    localStorage.setItem("done", JSON.stringify(arrDone.concat(["false"])));
   }
   removeAll() {
     localStorage.setItem("tasks", "[]");
-    this.setState({ text: [] });
+    localStorage.setItem("done", "[]");
+
+    this.setState({ text: [], done: [] });
   }
 
   removecompleted() {
     document.querySelectorAll("delcompleted");
   }
+  signTask = function (index) {
+    console.log(this);
+    let doneArr = [...this.state.done];
+    doneArr[index] = "true";
+    this.setState({ done: doneArr });
+  };
 
   render() {
     return (
       <div>
         <Form
-          add={this.AddTask.bind(this)}
+          add={this.addTask.bind(this)}
           delAll={this.removeAll.bind(this)}
         ></Form>
         {this.state.text.map((element, index) => {
-          return <Task key={index} text={element}></Task>;
+          return (
+            <Task
+              key={index}
+              text={element}
+              done={this.state.done[index]}
+              vBtn={this.signTask}
+              id={index}
+            ></Task>
+          );
         })}
       </div>
     );
--- src/components/tasks/tasks.jsx
+++ src/components/tasks/tasks.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 export default function Task(props) {
-  return (
-    <div className="row flex">
+  let temp = 0;
+  if (props.done == "false") {
+    temp = (
       <span className="text w-80 bg-white ml-2.5 mt-3 text-left pl-2">
         {props.text}
       </span>
+    );
+  } else {
+    temp = (
+      <span className="text w-80 bg-green-100 line-through ml-2.5 mt-3 text-left pl-2">
+        {props.text}
+      </span>
+    );
+  }
+  return (
+    <div className="row flex">
+      {temp}
       <button
         className="v-button mt-3 bg-green-300 w-8 text-white"
-        onClick={props.completeTask}
+        onClick={() => props.vBtn(props.id)}
       >
         &#10003;
       </button>
